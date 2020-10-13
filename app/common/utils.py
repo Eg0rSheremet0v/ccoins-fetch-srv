0489410313e721f3c23a3f5b9432df0e3f6f820f
egor_sheremetov <georgesheremetov3.0@gmail.com>
2020-10-13 13:08:40 +0300

    date validation, human friendly errors

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,6 +1,14 @@
 from app.common.objects import CoinsCurrency, CurrencyMap
 from binance.client import Client
 
+from datetime import datetime
+
+class IntervalException(Exception):
+    pass
+
+class DateFormatException(Exception):
+    pass
+
 class Transform():
     
     @staticmethod
@@ -14,10 +22,24 @@ class Transform():
         return data.get(interval.upper())
     
     @staticmethod
+    def _validate_date(date_start, date_end):
+        try:
+            first_date = datetime.strptime(date_start, '%d %b, %Y')
+            second_date = datetime.strptime(date_end, '%d %b, %Y')
+            if second_date >= first_date:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+        
+    @staticmethod
     def _get_data(cur_data):
         currency_name = ''.join([cur_data.base_coin, cur_data.target_coin])
         cur_obj = CoinsCurrency(currency_name)
         interval = Transform._transform_interval(cur_data.interval)
+        if not interval:
+            raise IntervalException('Unknown interval!')
         k_lines = cur_obj.historical_data(interval, cur_data.start_time, cur_data.end_time)
         close_data = [kl.close_data for kl in k_lines]
         return close_data
@@ -25,6 +47,8 @@ class Transform():
     @staticmethod
     def create_map(data):
         cur_name = ''.join([data.base_coin, data.target_coin])
+        if not Transform._validate_date(data.start_time, data.end_time):
+            raise DateFormatException('Date format error. Check start_time or end_time.')
         close_data = Transform._get_data(data)
         if close_data:
             heatmap = CurrencyMap(cur_name)
